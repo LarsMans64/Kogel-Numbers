6b86b3759eedb8ddb4f75259649458f90fc6ac9c
LarsMans <l.s.vanemmerik@gmail.com>
2023-02-02 18:11:56 +0100

    Raised shift value and added output message

---
+++ README.md
@@ -0,0 +1,17 @@
+# Kogel Numbers
+
+This is a little script for finding 'Kogel numbers'.
+
+A Kogel number is a number which value is the same as the index of its place in another number like π.  
+For example, in the number 67042, 4 is a Kogel number because it's the fourth digit.
+
+The output will look like this:
+```commandline
+Shift : Number
+1 : 1
+1 : 16
+2 : 45
+1 : 61
+1 : 75
+```
+The first number 
--- main.py
+++ main.py
@@ -1,38 +1,50 @@
+import time
+
+
 def start():
-    file = open(input("File: ") + ".txt")
+    file_name = input("File: ")
+    try:
+        file = open(file_name + ".txt")
+    except FileNotFoundError:
+        file = open(file_name)
     count = int(input("How many digits? (0 = all) "))
-    print("")
-    print("Shift : Number")
-    if count == 0: count = len(file.read())
+    if count == 0:
+        count = len(file.read())
+    print()
+    print("Digit : Number")
+    start_time = time.time()
     main(file, count)
+    end_time = time.time()
+    print(f"Checked {count:,} digits in {file.name} in {round(end_time - start_time, 2)} seconds.")
+    print()
+    start()
 
 
 def main(file, count):
     index = 0
-    while index <= count:   # Check all digits
+    while index <= count:  # Check all digits
         length = len(str(index))
         if index + length >= count: break
         shift = 0
-        while shift < length:   # Check all possible shifts
+        while shift < length:  # Check all possible shifts
             index2 = index + shift
             digit = 0
-            while digit < length:   # Check all digits of the number
+            while digit < length:  # Check all digits of the number
                 temp_index = index + digit
                 file.seek(temp_index)
                 if int(file.read(1)) != int(str(index2)[digit]):
                     break
                 if digit + 1 == length:
-                    print(shift, ":", index2)
+                    print(shift + 1, ":", index2)
                 digit += 1
             shift += 1
         index += 1
-    print()
-    start()
+    return
 
 
 print("""
 Kogel Numbers
 -------------
-Put your files in the same folder as the script
+Put your files in the same folder as the script.
 """)
 start()
