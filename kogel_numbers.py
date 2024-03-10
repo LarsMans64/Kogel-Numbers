4bb80bbafdaf9ebb72ac59829666413e3277812b
LarsMans <l.s.vanemmerik@gmail.com>
2024-03-10 23:44:51 +0100

    insane performance

--- kogel_numbers.py
+++ kogel_numbers.py
@@ -10,17 +10,19 @@ def start():
     except FileNotFoundError:
         file = open(file_name)
 
+    file_str = file.read()
+
     count = int(input("How many digits? (0 = all) "))
-    if count == 0: count = len(file.read())
+    if count == 0: count = len(file_str)
 
     filter_digit = int(input("Search for which digit per number? (0 = all, -1 = last) ")) - 1
 
     start_time = time.time()
     print("\nDigit : Number")
     if filter_digit == -1:
-        main(file, count)
+        main(file_str, count)
     else:
-        main_filtered(file, count, filter_digit)
+        main_filtered(file_str, count, filter_digit)
     end_time = time.time()
 
     fcount = f"{count:,}"
@@ -33,7 +35,64 @@ def start():
     start()
 
 
-def main(file, count):
+def main(file: str, count: int):
+    index = 0
+    bar_number = count // 531
+    while index <= count:  # Check all digits in file
+        if index % bar_number == 0:
+            update_progress_bar(index, count)
+        length = len(str(index))
+        if index + length >= count:
+            break
+        shift = 0
+        while shift < length:  # Check all possible shifts
+            index_with_shift = index + shift
+            digit = 0
+            while digit < length:  # Check all digits of the number
+                if file[index + digit] != str(index_with_shift)[digit]:
+                    break
+                if digit == length - 1:
+                    print("\r" + " " * 33, " " * 2 * len(f"{count:,}"), end="\r")
+                    print(f"    {shift + 1} : {index_with_shift}")
+                digit += 1
+            shift += 1
+        index += 1
+    return
+
+
+def main_filtered(file: str, count: int, filter_digit: int):
+    index = 0
+    bar_number = count // 531
+    while index <= count:
+        if index % bar_number == 0:
+            update_progress_bar(index, count)
+        length = len(str(index)) - 1
+        if filter_digit == -2:
+            shift = length
+        else:
+            shift = filter_digit
+        if index + length > count:
+            break
+        index_with_shift = index + shift
+        digit = 0
+        while digit <= length and filter_digit <= length:
+            if file[index + digit] != str(index_with_shift)[digit]:
+                break
+            if digit == length:
+                print("\r" + " " * 33, " " * 2 * len(f"{count:,}"), end="\r")
+                print(f"    {shift + 1} : {index_with_shift}")
+            digit += 1
+        index += 1
+    return
+
+
+def update_progress_bar(value, total):
+    a = "#" * int(value / total * 30)
+    b = "." * (30 - len(a))
+    print(f"\r[{a + b}] {value:,}/{total:,}", end="")
+
+
+def main_slow(file, count):
     index = 0
     bar_number = count // 531
     while index <= count:  # Check all digits in file
@@ -59,40 +118,11 @@ def main(file, count):
     return
 
 
-def main_filtered(file, count, filter_digit):
-    index = 0
-    bar_number = count // 531
-    while index <= count:
-        if index % bar_number == 0:
-            update_progress_bar(index, count)
-        length = len(str(index)) - 1
-        if filter_digit == -2: shift = length
-        else: shift = filter_digit
-        if index + length > count: break
-        index_with_shift = index + shift
-        digit = 0
-        while digit <= length and filter_digit <= length:
-            file.seek(index + digit)
-            if int(file.read(1)) != int(str(index_with_shift)[digit]):
-                break
-            if digit == length:
-                print("\r" + " " * 33, " " * 2 * len(f"{count:,}"), end="\r")
-                print(f"    {shift + 1} : {index_with_shift}")
-            digit += 1
-        index += 1
-    return
-
-
-def update_progress_bar(value, total):
-    a = "#" * int(value / total * 30)
-    b = "." * (30 - len(a))
-    print(f"\r[{a + b}] {value:,}/{total:,}", end="")
-
-
-print("""
+if __name__ == '__main__':
+    print("""
 Kogel Numbers
 -------------
 Put your files in the /files/ folder.
 The file can only contain numbers and nothing else.
 Type 'stop' to exit.""")
-start()
+    start()
